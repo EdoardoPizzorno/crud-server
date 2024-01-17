567772625bd1d76f18ab39ca2f2dc030f1c129ea
Edoardo Pizzorno <92369419+EdoardoPizzorno@users.noreply.github.com>
2024-01-17 13:19:16 +0100

    Change collection name

--- server.ts
+++ server.ts
@@ -6,35 +6,36 @@ import _dotenv from 'dotenv'
 import _cors from "cors";
 
 //lettura delle password
-_dotenv.config({"path":".env"})
+_dotenv.config({ "path": ".env" })
 
 //MongoDB ed express
-import {MongoClient, ObjectId} from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 
-const DBNAME =process.env.DBNAME
-const connectionString=process.env.CONNECTION_STRING;
-const app=_express()
+const DBNAME = process.env.DBNAME
+const connectionString = process.env.CONNECTION_STRING;
+const app = _express()
 
 
-const port:number=parseInt(process.env.PORT)
-let paginaErrore:any
-const server:any=_http.createServer(app)
+const port: number = parseInt(process.env.PORT)
+let paginaErrore: any
+const server: any = _http.createServer(app)
 
+const collectionName = "recipes"
 
 //se si specifica l'ip, si mette il server in ascolto su una singola interfaccia
 //se viene omesso il server è in ascolto su tutte le 3 interfaccie (loopback,scheda ethernet, scheda wi-fi)
 
-server.listen(port,()=>{
+server.listen(port, () => {
     init()
-    console.log("Il server è attivo sulla porta: "+port)
+    console.log("Il server è attivo sulla porta: " + port)
 })
 
-function init(){
-    _fs.readFile("./static/error.html",function(err,data){
-        if(err)
-            paginaErrore="<h1>Risorsa non trovata</h1>"
+function init() {
+    _fs.readFile("./static/error.html", function (err, data) {
+        if (err)
+            paginaErrore = "<h1>Risorsa non trovata</h1>"
         else
-            paginaErrore=data.toString()
+            paginaErrore = data.toString()
     })
 }
 
@@ -44,26 +45,26 @@ function init(){
  * ****************************************************************************************************************/
 
 //1. Request log
-app.use("/",(req:any,res:any,next:any)=>{
-    console.log("------> "+req.method+":"+req.originalUrl);
+app.use("/", (req: any, res: any, next: any) => {
+    console.log("------> " + req.method + ":" + req.originalUrl);
     next()
 })
 
 
 //2. Gestione risorse statiche
-app.use("/",_express.static("./static"))
+app.use("/", _express.static("./static"))
 
 
 //3. Lettura parametri body
-app.use("/",_express.json({"limit":"50mb"}))
-app.use("/",_express.urlencoded({"limit":"50mb","extended":true}))
+app.use("/", _express.json({ "limit": "50mb" }))
+app.use("/", _express.urlencoded({ "limit": "50mb", "extended": true }))
 
 //4. Log parameters GET and POST
-app.use("/",(req:any,res:any,next:any)=>{
-    if(Object.keys(req["query"]).length>0)
-        console.log("        "+JSON.stringify(req["query"]))
-    if(Object.keys(req["body"]).length>0)
-        console.log("        "+JSON.stringify(req["body"]))
+app.use("/", (req: any, res: any, next: any) => {
+    if (Object.keys(req["query"]).length > 0)
+        console.log("        " + JSON.stringify(req["query"]))
+    if (Object.keys(req["body"]).length > 0)
+        console.log("        " + JSON.stringify(req["body"]))
     next()
 })
 
@@ -79,118 +80,117 @@ app.use("/", _cors(corsOptions));
 /*********************************************************************************************************************************** */
 //Route finali risposta al client
 /*********************************************************************************************************************************** */
-app.get("/api/richiesta1",async(req,res,next)=>{
+app.get("/api/richiesta1", async (req, res, next) => {
     const client = new MongoClient(connectionString);
     await client.connect()//apre la connessione
-    let nome:string=req["query"]["nome"]
-    let collection:any = client.db(DBNAME).collection("unicorns");
-    let rq:any=collection.findOne({"name":nome})
-    rq.then((data)=>{
+    let nome: string = req["query"]["nome"]
+    let collection: any = client.db(DBNAME).collection(collectionName);
+    let rq: any = collection.findOne({ "name": nome })
+    rq.then((data) => {
         res.send(data)
     })
-    rq.catch((err)=>{
+    rq.catch((err) => {
         res.status(500)
-        res.send("Errore esecuzione query: "+err)
+        res.send("Errore esecuzione query: " + err)
     })
-    rq.finally(()=>{
+    rq.finally(() => {
         client.close()
     })
 })
 
-app.get("/api/getCollections",async(req,res,next)=>{
-    const client=new MongoClient(connectionString)
+app.get("/api/getCollections", async (req, res, next) => {
+    const client = new MongoClient(connectionString)
     await client.connect()
-    let db=client.db(DBNAME)
-    let request=db.listCollections().toArray()
-    request.then((data)=>{
+    let db = client.db(DBNAME)
+    let request = db.listCollections().toArray()
+    request.then((data) => {
         res.send(data)
     })
-    request.catch((err)=>{
-        res.status(500).send("Errore lettura collezioni: "+err)
+    request.catch((err) => {
+        res.status(500).send("Errore lettura collezioni: " + err)
     })
-    request.finally(()=>{
+    request.finally(() => {
         client.close()
     })
 })
 
-app.get("/api/:collection",async(req,res,next)=>{
-    let filters=req["query"]
+app.get("/api/:collection", async (req, res, next) => {
+    let filters = req["query"]
     //console.log(filters)
-    const client=new MongoClient(connectionString)
+    const client = new MongoClient(connectionString)
     await client.connect()
-    let db=client.db(DBNAME).collection(req.params.collection)
-    let request=db.find(filters).toArray()
-    request.then((data)=>{
+    let db = client.db(DBNAME).collection(req.params.collection)
+    let request = db.find(filters).toArray()
+    request.then((data) => {
         res.send(data)
     })
-    request.catch((err)=>{
-        res.status(500).send("Errore lettura collezioni: "+err)
+    request.catch((err) => {
+        res.status(500).send("Errore lettura collezioni: " + err)
     })
-    request.finally(()=>{
+    request.finally(() => {
         client.close()
     })
 })
-app.get("/api/:collection/:id",async(req,res,next)=>{
-    let collection=req["params"].collection
-    let id=req["params"]["id"]
+app.get("/api/:collection/:id", async (req, res, next) => {
+    let collection = req["params"].collection
+    let id = req["params"]["id"]
     let objId
-    if(ObjectId.isValid(id))
-    {
-        objId=new ObjectId(req["params"].id)
+    if (ObjectId.isValid(id)) {
+        objId = new ObjectId(req["params"].id)
     }
     else
-        objId=id as unknown as ObjectId
-    const client=new MongoClient(connectionString)
+        objId = id as unknown as ObjectId
+    const client = new MongoClient(connectionString)
     await client.connect()
-    let db=client.db(DBNAME).collection(collection)
-    let request=db.findOne({"_id":objId})
-    request.then((data)=>{
+    let db = client.db(DBNAME).collection(collection)
+    let request = db.findOne({ "_id": objId })
+    request.then((data) => {
         res.send(data)
     })
-    request.catch((err)=>{
-        res.status(500).send("Errore esecuzione query: "+err)
+    request.catch((err) => {
+        res.status(500).send("Errore esecuzione query: " + err)
     })
-    request.finally(()=>{
+    request.finally(() => {
         client.close()
     })
 })
-app.post("/api/:collection",async(req,res,next)=>{
-    let collection=req["params"].collection
-    let newRecord=req["body"]
-    const client=new MongoClient(connectionString)
+app.post("/api/:collection", async (req, res, next) => {
+    let collection = req["params"].collection
+    let newRecord = req["body"]
+    const client = new MongoClient(connectionString)
     await client.connect()
-    let db=client.db(DBNAME).collection(collection)
-    let request=db.insertOne(newRecord)
-    request.then((data)=>{
+    let db = client.db(DBNAME).collection(collection)
+    let request = db.insertOne(newRecord)
+    request.then((data) => {
         res.send(data)
     })
-    request.catch((err)=>{
-        res.status(500).send("Errore esecuzione query: "+err)
+    request.catch((err) => {
+        res.status(500).send("Errore esecuzione query: " + err)
     })
-    request.finally(()=>{
+    request.finally(() => {
         client.close()
     })
 })
 
-app.delete("/api/:collection/:id",async(req,res,next)=>{
-    let collection=req["params"].collection
-    let id=req["params"]["id"]
+app.delete("/api/:collection/:id", async (req, res, next) => {
+    let collection = req["params"].collection
+    let id = req["params"]["id"]
     let objId
-    if(ObjectId.isValid(id))
-        objId=new ObjectId(req["params"].id)
+    if (ObjectId.isValid(id))
+        objId = new ObjectId(req["params"].id)
     else
-        objId=id as unknown as ObjectId
-    const client=new MongoClient(connectionString)
+        objId = id as unknown as ObjectId
+    const client = new MongoClient(connectionString)
     await client.connect()
-    let db=client.db(DBNAME).collection(collection)
-    let request=db.deleteOne({"_id":objId})
-    request.then((data)=>{
+    let db = client.db(DBNAME).collection(collection)
+    let request = db.deleteOne({ "_id": objId })
+    request.then((data) => {
         res.send(data)
     })
-    request.catch((err)=>{
-        res.status(500).send("Errore esecuzione query: "+err)
+    request.catch((err) => {
+        res.status(500).send("Errore esecuzione query: " + err)
     })
-    request.finally(()=>{
+    request.finally(() => {
         client.close()
     })
 })
@@ -206,26 +206,26 @@ app.delete("/api/:collection", async (req, res, next) => {
     rq.finally(() => client.close());
 });
 
-app.patch("/api/:collection/:id",async(req,res,next)=>{
-    let collection=req["params"].collection
-    let id=req["params"]["id"]
+app.patch("/api/:collection/:id", async (req, res, next) => {
+    let collection = req["params"].collection
+    let id = req["params"]["id"]
     let objId
-    if(ObjectId.isValid(id))
-        objId=new ObjectId(req["params"].id)
+    if (ObjectId.isValid(id))
+        objId = new ObjectId(req["params"].id)
     else
-        objId=id as unknown as ObjectId
-    let action=req["body"]
-    const client=new MongoClient(connectionString)
+        objId = id as unknown as ObjectId
+    let action = req["body"]
+    const client = new MongoClient(connectionString)
     await client.connect()
-    let db=client.db(DBNAME).collection(collection)
-    let request=db.updateOne({"_id":objId},action)
-    request.then((data)=>{
+    let db = client.db(DBNAME).collection(collection)
+    let request = db.updateOne({ "_id": objId }, action)
+    request.then((data) => {
         res.send(data)
     })
-    request.catch((err)=>{
-        res.status(500).send("Errore esecuzione query: "+err)
+    request.catch((err) => {
+        res.status(500).send("Errore esecuzione query: " + err)
     })
-    request.finally(()=>{
+    request.finally(() => {
         client.close()
     })
 })
@@ -243,26 +243,26 @@ app.patch("/api/:collection", async (req, res, next) => {
     rq.finally(() => client.close());
 });
 
-app.put("/api/:collection/:id",async(req,res,next)=>{
-    let collection=req["params"].collection
-    let id=req["params"]["id"]
+app.put("/api/:collection/:id", async (req, res, next) => {
+    let collection = req["params"].collection
+    let id = req["params"]["id"]
     let objId
-    if(ObjectId.isValid(id))
-        objId=new ObjectId(req["params"].id)
+    if (ObjectId.isValid(id))
+        objId = new ObjectId(req["params"].id)
     else
-        objId=id as unknown as ObjectId
-    let updatedRecord=req["body"]
-    const client=new MongoClient(connectionString)
+        objId = id as unknown as ObjectId
+    let updatedRecord = req["body"]
+    const client = new MongoClient(connectionString)
     await client.connect()
-    let db=client.db(DBNAME).collection(collection)
-    let request=db.replaceOne({"_id":objId},updatedRecord)
-    request.then((data)=>{
+    let db = client.db(DBNAME).collection(collection)
+    let request = db.replaceOne({ "_id": objId }, updatedRecord)
+    request.then((data) => {
         res.send(data)
     })
-    request.catch((err)=>{
-        res.status(500).send("Errore esecuzione query: "+err)
+    request.catch((err) => {
+        res.status(500).send("Errore esecuzione query: " + err)
     })
-    request.finally(()=>{
+    request.finally(() => {
         client.close()
     })
 })
@@ -271,14 +271,14 @@ app.put("/api/:collection/:id",async(req,res,next)=>{
 /***************************************************************************** *************************************************************/
 //Default route e gestione degli errori
 /************************************************************************************************************************************************ */
-app.use("/",(req,res,next)=>{
+app.use("/", (req, res, next) => {
     res.status(404)
-    if(req.originalUrl.startsWith("/api/"))
+    if (req.originalUrl.startsWith("/api/"))
         res.send("API non disponibile")
     else
         res.send(paginaErrore)
 })
-app.use("/",(err,req,res,next)=>{
+app.use("/", (err, req, res, next) => {
     console.log("************* SERVER ERROR ***************\n", err.stack)
     res.status(500).send(err.message)
 })
